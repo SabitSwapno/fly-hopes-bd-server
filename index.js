63c8a1be1435989f4d182718b3e3685e1d7ed7cf
SabitSwapno <sabitswapno3@gmail.com>
2021-10-31 23:22:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.izxnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("flyhopesBD");
+        const servicesCollection = database.collection("services");
+        const orderCollection = database.collection('orders');
+
+
+        // get api 
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // get order api
+        app.get('/orders', async (req, res) => {
+            const curs = orderCollection.find({});
+            const orders = await curs.toArray();
+            res.send(orders);
+        })
+
+        // Get single service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        // post api 
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        })
+
+        // add order's api
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        })
+
+        // delete orders
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running my crud server');
+});
+
+app.listen(port, () => {
+    console.log('running server on port', port);
+})
